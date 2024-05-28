86f808f89c0cca38f3af38a9340685659a17cf66
gokul <infinest.tech@gmail.com>
2024-05-28 18:48:39 +0530

    Resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
-// tailwind.config.js
 module.exports = {
   mode: 'jit', // Just-In-Time Compiler
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-   fontFamily:{
-    poppins:['Open Sans", sans-serif']
-   }
+    fontFamily: {
+      poppins: ['"Open Sans"', 'sans-serif']
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
-
- 
 };
