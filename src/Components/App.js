31d16c2afa979ab050aeb3050524e1ebbf6a5228
gokul <infinest.tech@gmail.com>
2024-05-29 22:41:46 +0530

    gokul testing

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-//import ParticleBackground from './ParticleBackground';
-//import SpaceScene from './SpaceScene'; // Make sure the path to SpaceScene is correct
-import "../css/App.css"
- import About from "./About";
- import Home from "./Home"
- import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
- import Combine from './Combine';
- import ContactForm from './ContactForm';
-
+// import ParticleBackground from './ParticleBackground';
+// import SpaceScene from './SpaceScene'; // Make sure the path to SpaceScene is correct
+import "../css/App.css";
+import About from "./About";
+import Home from "./Home";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Combine from './Combine';
+import ContactForm from './ContactForm';
 
 const App = () => {
   // const [entered, setEntered] = useState(false);
@@ -17,36 +16,26 @@ const App = () => {
   // };
 
   return (
-
-   
     <div>
-    
-      
-     {/* <div className='spaceSceneContainer'> */}
-      {/* <SpaceScene /> SpaceScene is included and will render when App loads
-      
-      </div> */}
+      {/* <div className='spaceSceneContainer'> */}
+      {/* <SpaceScene /> SpaceScene is included and will render when App loads */}
+      {/* </div> */}
      
       <Router>  
-         <Routes>
-        <Route path="/infinestech" element={<Combine />}/>
-          <Route path="/home" element={ <Home />} />
-          <Route path="/careers" element={ <ContactForm />} />
-          <Route path="/about" element={ <About />} /> 
-         </Routes>
-         </Router>
+        <Routes>
+          <Route path="/" element={<Combine />} />
+          <Route path="home" element={<Home />} />
+          <Route path="careers" element={<ContactForm />} />
+          <Route path="about" element={<About />} /> 
+        </Routes>
+      </Router>
       
-      
-   
       {/* ParticleBackground is now below SpaceScene, both could be visible */}
-       {/* <ParticleBackground /> */}
+      {/* <ParticleBackground /> */}
       
-       {/* Conditionally render HomeScreen or MainScreen based on 'entered' state */}
-     
-     </div>
+      {/* Conditionally render HomeScreen or MainScreen based on 'entered' state */}
+    </div>
   );
 }
 
 export default App;
-
-
